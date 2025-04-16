6c697247249c525e493e6d7994dc5fec3fe247f9
julien2189 <maitreobscur89@gmail.com>
2025-04-17 00:35:09 +0200

    Premier commit

---
+++ script.js
@@ -0,0 +1,45 @@
+async function getBTCPrice() {
+    try {
+      const response = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+      const data = await response.json();
+      const price = parseFloat(data.price);
+      document.getElementById('btc-price').textContent = price.toLocaleString();
+    } catch (error) {
+      document.getElementById('btc-price').textContent = "Erreur";
+      console.error(error);
+    }
+  }
+
+  getBTCPrice();
+  setInterval(getBTCPrice, 1000); // toutes les 5 secondes
+
+  async function getXRPPrice() {
+    try {
+      const response = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=XRPUSDT');
+      const data = await response.json();
+      const price = parseFloat(data.price);
+      document.getElementById('xrp-price').textContent = price.toLocaleString();
+    } catch (error) {
+      document.getElementById('xrp-price').textContent = "Erreur";
+      console.error(error);
+    }
+  }
+
+  getXRPPrice();
+  setInterval(getXRPPrice, 1000); // toutes les 5 secondes
+
+  
+  async function getETHPrice() {
+    try {
+      const response = await fetch('https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT');
+      const data = await response.json();
+      const price = parseFloat(data.price);
+      document.getElementById('eth-price').textContent = price.toLocaleString();
+    } catch (error) {
+      document.getElementById('eth-price').textContent = "Erreur";
+      console.error(error);
+    }
+  }
+
+  getETHPrice();
+  setInterval(getETHPrice, 1000); // toutes les 5 secondes
